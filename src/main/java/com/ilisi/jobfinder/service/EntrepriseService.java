30f28f11bc342c4fea7eae3cfe46afe5710b796a
charafzi <ghaziachraf99@gmail.com>
2024-12-27 23:24:51 +0100

    Add picture handling for Entreprise

--- src/main/java/com/ilisi/jobfinder/service/EntrepriseService.java
+++ src/main/java/com/ilisi/jobfinder/service/EntrepriseService.java
@@ -13,14 +13,25 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Service
 @AllArgsConstructor
 public class EntrepriseService {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final String uploadDir = "src/main/java/com/ilisi/jobfinder/assets/entrepriseProfilePic";
+    private final Path storageLocation = Paths.get(System.getProperty("user.dir"), uploadDir);
 
     public User registerEntreprise(RegisterEntrepriseRequest entrepriseRequest) throws EmailAlreadyExists{
         if (userService.getUserByEmail(entrepriseRequest.getEmail()).isPresent()) {
@@ -73,5 +84,69 @@ public class EntrepriseService {
 
     public void deleteEntrepriseByEmail(String email){
         this.userService.deleteUserByEmail(email);
+    }
+
+    public String uploadProfilePicture(String email, MultipartFile file) throws IOException {
+        // Retrieve the user from the database
+        User user = userService.getUserByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        //ensure directory exists
+        Files.createDirectories(storageLocation);
+
+        try (Stream<Path> files = Files.list(storageLocation)) {
+            files
+                    .filter(path -> path.getFileName().toString().startsWith("profile_" + email+ "_"))
+                    .forEach(existingFile -> {
+                        try {
+                            Files.delete(existingFile);
+                        } catch (IOException e) {
+                            System.err.println("Failed to delete old profile picture: " + existingFile);
+                        }
+                    });
+        }
+
+        //generate user profile url
+        String originalFilename = file.getOriginalFilename();
+        // Extract the file extension
+        String fileExtension = "";
+        if (originalFilename != null && originalFilename.contains(".")) {
+            fileExtension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        }
+        LocalDateTime now = LocalDateTime.now();
+        // Format the timestamp
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+        String timestamp = now.format(formatter);
+        // Construct the user profile URL with only the file extension
+        String userProfileUrl = "profile_" + email + "_" + timestamp + fileExtension;
+        Path filePath = storageLocation.resolve(userProfileUrl);
+
+        //save file on server locally
+        Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+        // Update the user's profile picture URL in the database
+        user.setPhotoProfile(userProfileUrl);
+        userService.saveUser(user);
+        return userProfileUrl;
+    }
+
+    public byte[] getProfilePictureData(String email) throws IOException {
+        User user = userService.getUserByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        // Get the URL or file path of the stored image
+        String imageUrl = user.getPhotoProfile();
+        System.out.println("image url : "+imageUrl);
+        if (imageUrl == null || imageUrl.isEmpty()) {
+            return null;
+        }
+
+        Path imagePath = storageLocation.resolve(imageUrl);
+
+        if (!Files.exists(imagePath)) {
+            return null;
+        }
+
+        return Files.readAllBytes(imagePath);
     }
 }
