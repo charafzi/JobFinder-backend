027ad515dac0fbe5a28979b3ddfbccccdac767d5
charafzi <ghaziachraf99@gmail.com>
2025-01-12 13:32:08 +0100

    Add webSocketIO to broadcast created offre for real time offre displaying

--- src/main/java/com/ilisi/jobfinder/dto/AdresseDTO.java
+++ src/main/java/com/ilisi/jobfinder/dto/AdresseDTO.java
@@ -1,12 +1,16 @@
 package com.ilisi.jobfinder.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.Nullable;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdresseDTO {
     private String city;
     private String adress;
